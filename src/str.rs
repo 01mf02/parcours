83e2230730fe747cc8b81c71e2f53ac242fc409f
Michael Färber <01mf02@gmail.com>
2023-12-20 16:40:03 +0100

    Make a function return something cloneable.

--- src/str.rs
+++ src/str.rs
@@ -1,9 +1,9 @@
 //! Parsers for [`&str`] input.
 
-use crate::{FromFn, Parser};
+use crate::{from_fn, Parser};
 
 pub fn take_while0<'a, S>(mut f: impl FnMut(&u8) -> bool) -> impl Parser<&'a str, S, O = &'a str> {
-    FromFn(move |input: &'a str, _state: &mut S| {
+    from_fn(move |input: &'a str, _state: &mut S| {
         let len = input.bytes().take_while(|c| f(c)).count();
         Some((&input[..len], &input[len..]))
     })
@@ -13,6 +13,6 @@ pub fn take_while1<'a, S>(f: impl FnMut(&u8) -> bool) -> impl Parser<&'a str, S,
     take_while0(f).filter(|n| !n.is_empty())
 }
 
-pub fn matches<'a, 'i: 'a, S>(x: &'a str) -> impl Parser<&'i str, S, O = ()> + 'a {
-    FromFn(move |input: &'i str, _state: &mut S| input.strip_prefix(x).map(|rest| ((), rest)))
+pub fn matches<'a, 'i: 'a, S>(x: &'a str) -> impl Parser<&'i str, S, O = ()> + Clone + 'a {
+    from_fn(move |input: &'i str, _state: &mut S| input.strip_prefix(x).map(|rest| ((), rest)))
 }
