0c76fe0d0dd7db01e3d567e7d5e442027ff5e0cc
Michael Färber <01mf02@gmail.com>
2024-01-03 12:37:04 +0100

    Remove unused lifetimes.

--- src/str.rs
+++ src/str.rs
@@ -3,7 +3,7 @@
 use crate::{from_fn, Combinator, Parser};
 
 /// Collect longest prefix of a [`&str`] whose bytes satisfies the given condition.
-pub fn take_while<'a, S, F: FnMut(&u8, &mut S) -> bool>(f: F) -> TakeWhile<F> {
+pub fn take_while<S, F: FnMut(&u8, &mut S) -> bool>(f: F) -> TakeWhile<F> {
     TakeWhile(f)
 }
 
@@ -21,7 +21,7 @@ impl<'a, S, F: FnMut(&u8, &mut S) -> bool> Parser<&'a str, S> for TakeWhile<F> {
 
 type TakeWhile1<F> = crate::combinator::Filter<TakeWhile<F>, fn(&&str) -> bool>;
 
-pub fn take_while1<'a, S, F: FnMut(&u8, &mut S) -> bool>(f: F) -> TakeWhile1<F> {
+pub fn take_while1<S, F: FnMut(&u8, &mut S) -> bool>(f: F) -> TakeWhile1<F> {
     take_while(f).filter(|n| !n.is_empty())
 }
 
