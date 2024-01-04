ba1e498f03e4f1b5a4d95b8c6032d8c12b2002ba
Michael Färber <01mf02@gmail.com>
2024-01-04 10:23:07 +0100

    Syntax error reporting in lambda example.

--- examples/lambda.rs
+++ examples/lambda.rs
@@ -12,6 +12,7 @@ enum Error<'a> {
 struct State<'a> {
     vars: Vec<&'a str>,
     errs: Vec<Error<'a>>,
+    expected: Option<(&'static str, &'a str)>,
 }
 
 fn space<'a, S>() -> impl Parser<&'a str, S, O = &'a str> + Clone {
@@ -37,6 +38,13 @@ fn token<S>(x: &str) -> impl Parser<&str, S, O = ()> + Clone {
     matches(x).then_ignore(space())
 }
 
+fn expected<'a, O>(s: &'static str) -> impl Parser<&'a str, State<'a>, O = O> + Clone {
+    from_fn(move |input, state: &mut State| {
+        state.expected.get_or_insert((s, input));
+        None
+    })
+}
+
 fn atomic<'a>() -> impl Parser<&'a str, State<'a>, O = Term<'a>> + Clone {
     let var = ident().map_with(|v, state: &mut State<'a>| {
         let db = state.vars.iter().rev().position(|b| *b == v);
@@ -45,7 +53,7 @@ fn atomic<'a>() -> impl Parser<&'a str, State<'a>, O = Term<'a>> + Clone {
             0
         }))
     });
-    var.or(lazy!(term).delimited_by(token("("), token(")")))
+    var.or(lazy!(term).delimited_by(token("("), token(")").or(expected("closing parenthesis"))))
 }
 
 fn with_vars<'a, I, P>(vars: Vec<&'a str>, p: P) -> impl Parser<I, State<'a>, O = P::O>
@@ -62,7 +70,9 @@ where
 }
 
 fn term<'a>() -> impl Parser<&'a str, State<'a>, O = Term<'a>> {
-    let vars = ident().repeated().delimited_by(token("|"), token("|"));
+    let vars = ident()
+        .repeated()
+        .delimited_by(token("|"), token("|").or(expected("identifier or |")));
     let abst = vars.and_then(|vars: Vec<_>| {
         with_vars(vars.clone(), lazy!(term)).map(|t| (Term::Abst(vars, Box::new(t))))
     });
@@ -74,14 +84,16 @@ fn term<'a>() -> impl Parser<&'a str, State<'a>, O = Term<'a>> {
             Term::Appl(Box::new(head), args)
         }
     });
-    abst.or(appl).then_ignore(space())
+    abst.or(appl).then_ignore(space()).or(expected("term"))
 }
 
-fn main() {
-    let input = r#"(|ä y| (ä y) X0) x  "#;
+fn handle(input: &str) {
     let mut state = State::default();
     let output = term().parse(input, &mut state);
     println!("{:?}", output);
+    if let Some((e, location)) = state.expected {
+        eprintln!("Error: expected {e} at {location}");
+    }
     for e in state.errs {
         match e {
             Error::UnboundVar(v) => {
@@ -89,5 +101,12 @@ fn main() {
                 println!("Error: unbound variable \"{v}\" at byte {offset}");
             }
         }
+    }
+}
+
+fn main() {
+    //let input = r#"(|ä y| (ä y) X0) x  "#;
+    for line in std::io::stdin().lines() {
+        handle(&line.unwrap())
     }
 }
