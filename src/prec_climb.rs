3de6e17a418e9bea34d9b2ce2d585df49c1ac6c5
Michael Färber <01mf02@gmail.com>
2024-01-05 11:33:51 +0100

    Document precedence climbing.

--- src/prec_climb.rs
+++ src/prec_climb.rs
@@ -1,7 +1,18 @@
 //! Precedence climbing for parsing expressions with binary operators.
 //!
+//! This allows you to parse expressions that are
+//! separated by binary operators with precedence and associativity.
+//! For example, in the expression `1 + 2 * 3`, we usually want to
+//! parse this into `1 + (2 * 3)`, not `(1 + 2) * 3`.
+//! This is handled by saying that `*` has higher *precedence* than `+`.
+//! Also, when we have a power operator `^`, we want
+//! `2 ^ 3 ^ 4` to mean `(2 ^ 3) ^ 4`, not `2 ^ (3 ^ 4)`.
+//! This is handled by saying that `^` is *left-associative*.
+//!
 //! This was adapted from
 //! <https://ycpcs.github.io/cs340-fall2017/lectures/lecture06.html#implementation>.
+//!
+//! An example for simple arithmetic expressions follows:
 //!
 //! ~~~
 //! use parcours::prec_climb::{self, Associativity};
@@ -50,20 +61,29 @@
 
 use core::iter::Peekable;
 
+/// Associativity of an operator.
 pub enum Associativity {
+    /// `(x + y) + z`
     Left,
+    /// `x + (y + z)`
     Right,
 }
 
+/// Binary operator.
 pub trait Op {
+    /// "Stickiness" of the operator
     fn precedence(&self) -> usize;
+    /// Is the operator left- or right-associative?
     fn associativity(&self) -> Associativity;
 }
 
+/// An expression that can be built from other expressions with some operator.
 pub trait Expr<O: Op> {
+    /// Combine two expressions with an operator.
     fn from_op(lhs: Self, op: O, rhs: Self) -> Self;
 }
 
+/// Perform precedence climbing.
 pub fn climb<O: Op, T: Expr<O>>(head: T, iter: impl IntoIterator<Item = (O, T)>) -> T {
     climb1(head, &mut iter.into_iter().peekable(), 0)
 }
